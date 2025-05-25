919fd856cc1bbd48f88cf505483af32e5c780cca
Jakub Gania <contact@jakubgania.io>
2025-05-25 02:05:23 +0200

    Initial Commit

---
+++ GitHubClientTests/GitHubClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientTests.swift
+//  GitHubClientTests
+//
+//  Created by Jakub on 25.05.25.
+//
+
+import Testing
+@testable import GitHubClient
+
+struct GitHubClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
