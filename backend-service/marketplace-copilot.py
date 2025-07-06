b1492bfce8e4bf09338dfb228252094d96cd9ad8
Jakub Gania <contact@jakubgania.io>
2025-07-06 22:07:45 +0200

    update

--- backend-service/marketplace-copilot.py
+++ backend-service/marketplace-copilot.py
@@ -16,10 +16,30 @@ def scrape_marketplace_developers():
 
       for i in range(count):
         item = copilotExtensions.nth(i)
+        link = item.locator("h3 a")
+        href = link.get_attribute("href")
         title = item.locator("h3 a").inner_text()
+        has_verification = item.locator("h3 svg").count() > 0
+        description = item.locator("p").inner_text()
+
+        logo_img = item.locator('[data-testid="logo"] img')
+        logo_url = logo_img.get_attribute("src")
+
         print({
-            "title": title.strip(),
+          "href": href,
+          "logo": logo_url,
+          "title": title.strip(),
+          "verified": has_verification,
+          "description": description,
         })
+
+      # next_btn = page.locator('a[rel="next"]')
+      # if next_btn.count() > 0:
+      #     next_btn.click()
+      #     page.wait_for_load_state('networkidle')
+      # else:
+        # propably last page
+        
 
       browser.close()
   except Exception as e:
